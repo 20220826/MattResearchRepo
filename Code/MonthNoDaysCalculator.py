16b6c091c7c3527a9d6c641f15f14745e36125f1
20220826 <110362401+20220826@users.noreply.github.com>
2022-09-01 14:39:46 +1200

    Create MonthNoDaysCalculator.py

---
+++ Code/MonthNoDaysCalculator.py
@@ -0,0 +1,15 @@
+#Write a Python program to convert month name to a number of days.
+#Your program should look as follows: 
+#Input the name of Month: February                                  
+#No. of days: 28/29 days 
+
+
+month = (input("Input the name of the Month:"))
+
+if month in "February":
+    print ("No. of days: 28/29")
+elif month in ("April", "June", "September", "November"):
+    print ("No. of days: 30")
+elif month in ("January","March","May", "July", "August", "October", "December"):
+    print ("No. of days is: 31")
+else: print ("Please enter a month")
